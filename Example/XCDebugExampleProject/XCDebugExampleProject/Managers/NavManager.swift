848e6360dd88fa38c8118cf09193b191420b9910
Axel Ancona Esselmann <axel@anconaesselmann.com>
2023-11-15 22:14:17 -0800

    First exploration of navigating based on a debug setting

--- Example/XCDebugExampleProject/XCDebugExampleProject/Managers/NavManager.swift
+++ Example/XCDebugExampleProject/XCDebugExampleProject/Managers/NavManager.swift
@@ -2,14 +2,43 @@
 //
 
 import SwiftUI
+import Combine
 
 class NavManager: ObservableObject {
 
-    private init() {}
+    var bag = Set<AnyCancellable>()
 
     static let shared = NavManager()
 
     @Published
     @MainActor
-    var path = NavigationPath()
+    var path: [Screen] = []
+
+    private init() {
+        XCDebug.changed.sink {
+            self.updateAfterRemoteScreenChangeRequest()
+        }.store(in: &bag)
+
+    }
+
+    func updateAfterRemoteScreenChangeRequest() {
+        if let screen = XCDebug.get(\LoginDebug.anEnum) {
+            Task { @MainActor in
+                if let index = path.firstIndex(where: { existing in
+                    existing.rawValue == screen.rawValue
+                }) {
+                    if path.last != screen {
+                        path = Array(path[0...index])
+                    }
+                } else {
+                    path.append(screen)
+                }
+
+            }
+        } else {
+            Task { @MainActor in
+                path = []
+            }
+        }
+    }
 }
