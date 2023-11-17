6bd80c07ea6fb1d88485b1e41bccd66f360d7ec2
Axel Ancona Esselmann <axel@anconaesselmann.com>
2023-11-17 12:44:07 -0800

    Access to properties with subscript, convenience method for nilling, date bugfix

--- Sources/XcodeDebug/Supporting/SelfDescribingJson/SelfDescribingJson.swift
+++ Sources/XcodeDebug/Supporting/SelfDescribingJson/SelfDescribingJson.swift
@@ -41,8 +41,53 @@ public struct SelfDescribingJson {
         self.properties = properties
     }
 
-    private func mapValue(_ any: Any) -> Any {
-        guard 
+    public func data() throws -> Data {
+        var dict = JSON()
+        dict[.properties] = properties.reduce(into: JSON()) {
+            $0[$1.key] = Self.mapValue($1.value)
+        }
+        dict[.name] = name
+        dict[.key]  = key
+        return try JSONSerialization.data(withJSONObject: dict, options: [.sortedKeys, .prettyPrinted])
+    }
+
+    public func properties<T>(as type: T.Type) throws -> T
+        where T: Decodable
+    {
+        let data = try JSONSerialization.data(withJSONObject: properties, options: [.sortedKeys, .prettyPrinted])
+        return try DefaultCoders.decoder.decode(T.self, from: data)
+    }
+
+    public subscript<Value>(key: String) -> Value? {
+        get {
+            guard var value = properties[key] as? [String: Any] else {
+                return nil
+            }
+            return Self.unMapValue(value[.value])
+        }
+        set {
+            guard var value = properties[key] as? [String: Any] else {
+                return
+            }
+            if let updated = newValue {
+                value[.value] = updated
+            } else {
+                value[.value] = NSNull()
+            }
+            properties[key] = value
+        }
+    }
+
+    public mutating func nilValue(_ key: String) {
+        guard var value = properties[key] as? [String: Any] else {
+            return
+        }
+        value[.value] = NSNull()
+        properties[key] = value
+    }
+
+    private static func mapValue(_ any: Any) -> Any {
+        guard
             var dict = any as? JSON,
             let value = dict[.value]
         else {
@@ -63,20 +108,21 @@ public struct SelfDescribingJson {
         return dict
     }
 
-    public func data() throws -> Data {
-        var dict = JSON()
-        dict[.properties] = properties.reduce(into: JSON()) {
-            $0[$1.key] = mapValue($1.value)
+    private static func unMapValue<T>(_ any: Any?) -> T? {
+        switch T.self {
+        case is Date.Type:
+            guard 
+                let double = any as? Double,
+                let data = "\(double)".data(using: .utf8),
+                let date = try? JSONDecoder().decode(Date.self, from: data)
+            else {
+                // TODO: Decide if when decoding I map based on type string to avoid having
+                // either the mapped or unmapped based on edit
+                return any as? T
+            }
+            return date as? T
+        default:
+            return any as? T
         }
-        dict[.name] = name
-        dict[.key]  = key
-        return try JSONSerialization.data(withJSONObject: dict, options: [.sortedKeys, .prettyPrinted])
-    }
-
-    public func properties<T>(as type: T.Type) throws -> T
-        where T: Decodable
-    {
-        let data = try JSONSerialization.data(withJSONObject: properties, options: [.sortedKeys, .prettyPrinted])
-        return try DefaultCoders.decoder.decode(T.self, from: data)
     }
 }
